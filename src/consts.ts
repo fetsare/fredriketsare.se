fbb63e027794f30dc4f8f4273c5ae3a07e7a469c
Fredrik Etsare <fredrik@etsare.se>
2026-04-03 01:06:12 +0200

    refactored navbar and comma seperated it (sue me)

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,7 @@
 export const SITE_TITLE = "Fredrik Etsare";
 export const SITE_DESCRIPTION = "Fredrik Etsare";
+
+export const NAV_LINKS = [
+	{ href: "/", label: "Blog" },
+	{ href: "/projekt", label: "Mina Projekt" },
+] as const;
